4bec827cd0f4243bab51d5a15ab3f48510704308
設計者工作室Reayee <zen2362661@gmail.com>
2021-08-22 22:57:30 +0800

    FR

--- app.py
+++ app.py
@@ -4,6 +4,14 @@ myweb = Flask(__name__) # 代表目前執行的模組
 @myweb.route("/") # 函式的裝飾.供附加功能
 def home():
     return render_template("home1.html")
+
+@myweb.route("/resume") # 函式的裝飾.供附加功能
+def resume():
+    return render_template("resume.html")
+
+@myweb.route("/home") # 函式的裝飾.供附加功能
+def homet():
+    return render_template("home.html")
     
 @myweb.route("/text") # 函式的裝飾.供附加功能
 def text():
