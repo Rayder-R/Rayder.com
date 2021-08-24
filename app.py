347d2bfaf0fdbbf7505b07b95b996d45436a5f8d
設計者工作室Reayee <zen2362661@gmail.com>
2021-08-24 13:57:36 +0800

    FR

--- app.py
+++ app.py
@@ -12,14 +12,11 @@ def resume():
 @myweb.route("/work") # 函式的裝飾.供附加功能
 def work():
     return render_template("work.html")
+
+@myweb.route("/work2") # 函式的裝飾.供附加功能
+def work2():
+    return render_template("work2.html")
     
 
-
-
-@myweb.route("/text") # 函式的裝飾.供附加功能
-def text():
-    return render_template("text.html")
-
-
-if __name__ == "__main__":  # 如果以主程式執行
-    myweb.run()                # 立刻啟動伺服器
+if __name__ == "__main__":  # 主程式執行
+    myweb.run()                # 啟動伺服器
