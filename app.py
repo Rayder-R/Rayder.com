89dc1e3f6366960b2e72dd6a3807a6cc5fee09be
Rayder|曾瑞義 <zen2362661@gmail.com>
2022-09-09 00:13:16 +0800

    1

--- app.py
+++ app.py
@@ -1,25 +1,36 @@
-from flask import Flask, render_template
-myweb = Flask(__name__) # 代表目前執行的模組
+from flask import Flask, render_template, request
 
-@myweb.route("/") # 函式的裝飾.供附加功能
+
+myweb = Flask(__name__)  # 代表目前執行的模組
+
+
+
+@myweb.route("/")  # 函式的裝飾.供附加功能
 def home():
     return render_template("home1.html")
 
-@myweb.route("/resume") # 函式的裝飾.供附加功能
+
+@myweb.route("/resume")  # 函式的裝飾.供附加功能
 def resume():
     return render_template("resume.html")
 
-@myweb.route("/work") # 函式的裝飾.供附加功能
+
+@myweb.route("/work")  # 函式的裝飾.供附加功能
 def work():
+
+
     return render_template("work.html")
 
-@myweb.route("/work2") # 函式的裝飾.供附加功能
+
+@myweb.route("/work2")  # 函式的裝飾.供附加功能
 def work2():
     return render_template("work2.html")
 
-@myweb.route("/work3") # 函式的裝飾.供附加功能
-def work3():
-    return render_template("work3.html")
+
+# @myweb.route("/work3")  # 函式的裝飾.供附加功能
+# def work3():
+#     return render_template("work3.html")
+
 
 if __name__ == "__main__":  # 主程式執行
     myweb.run()                # 啟動伺服器
