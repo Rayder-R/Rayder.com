165660ad7ad138b6538c87290ff7d0686cd32e2d
Rayder-R <zen2362661@gmail.com>
2023-01-05 01:35:25 +0800

    first commit

--- app.py
+++ app.py
@@ -20,12 +20,12 @@ def work():
     return render_template("work.html")
 
 
-@myweb.route("/work2")  # 函式的裝飾.供附加功能 a
+@myweb.route("/work2")  # 函式的裝飾.供附加功能
 def work2():
     return render_template("work2.html")
 
 
-@myweb.route("/work3")  # 函式的裝飾.供附加功能.
+@myweb.route("/work3")  # 函式的裝飾.供附加功能
 def work3():
     return render_template("work3.html")
 
